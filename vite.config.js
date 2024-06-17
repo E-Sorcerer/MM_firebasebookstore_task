6e17a9eba1451d1e4f5a9406fc15e38b88e7ca74
markusmes <markusmestari@kamk.fi>
2024-06-18 02:04:25 +0300

    Fixed packages.json issue

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/firebasebookstore/', 
@@ -6,4 +9,4 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
   } 
-})
+});
